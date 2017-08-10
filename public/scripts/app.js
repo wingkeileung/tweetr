a7595eb3a70e95b5144bc51e1177b376bf49187a
Wing Kei Leung <winky.leung@gmail.com>
2017-08-10 15:30:31 +0000

    First commit for basic features and functions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,4 +3,130 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function() {
 
+function createTweetElement(tweet){
+
+  const name = tweet.user.name;
+  const avatars = tweet.user.avatars.small;
+  const handle = tweet.user.handle;
+  const $tweet =
+  `<article class="tweet">
+    <header>
+      <section class="avatar"><img src="${ avatars }"/></section>
+      <section class="tweetheader">${ name }</section>
+      <section class="uname">${ handle }</section>
+    </header>
+
+    <section class="maintweet">${escape(tweet.content.text)}</section>
+
+    <footer>
+      <div class="stamp"> ${(tweet.created_at) } </div>
+      <section class="tools">
+        <i class="fa fa-flag" aria-hidden="true"></i>
+        <i class="fa fa-refresh" aria-hidden="true"></i>
+        <i class="fa fa-heart" aria-hidden="true"></i>
+      </section>
+    </footer>
+    </article>`;
+
+    return $($tweet);
+}
+
+function escape(str) {
+  var div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
+
+$('form').submit(function(events) {
+  events.preventDefault(); // This prevent leaving the page for submit
+    var textChar = $(".tweetform").val().length;
+    if (textChar === null || textChar === 0 || textChar > 140){
+    // console.log(textChar);
+     alert ("Requirements not met!")
+    }
+    else {
+      $.ajax({
+        url: "/tweets",
+        method: "POST",
+        data: $(this).serialize()
+    }).done(function (){
+        $(".tweetform").val("");
+        alert ("Message Aceepted!");
+        loadTweets();
+    })
+  }
+});
+
+function loadTweets() {
+  $.ajax({
+    url: '/tweets',
+    method: 'GET'
+  })
+  .then((tweets) => { //change according to what's passed/received in data
+    $('#container-tweet').empty()  //empty container so that db doesn't duplicate when form is submitted
+    renderTweets(tweets)
+  })
+}
+
+function renderTweets(tweets){
+  for (tweet in tweets){
+    $('section.container-tweet').append(createTweetElement(tweets[tweet]));
+  }
+}
+
+$(".compose").click(function(){
+    $(".new-tweet").slideToggle();
+});
+
+var tweets = [
+    {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "<script>alert('uh oh!');</script>"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+]
+
+loadTweets();
+
+});
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+$(function() {
+
+  var maxLength = 140;
+
+  var $counter = $('#counter');
+
+  $('textarea').keyup(function() {
+
+    var wordCount = maxLength - $(this).val().length;
+    var color     = wordCount <= 0 ? 'red' : 'green';
+
+    $counter.text(wordCount);
+
+    if (wordCount <= 0) {
+      $(this).closest('.new-tweet').addClass('error');
+    } else {
+      $(this).closest('.new-tweet').removeClass('error');
+    }
+
+    });
+
+});
+
+// tweets.forEach(function (tweet) {
+//       $('section.container-tweet').append(createTweetElement(tweet));
+
+// });
+});
